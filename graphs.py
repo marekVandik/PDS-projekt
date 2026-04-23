59588b4c354c029e9e8d20084f92af1e273f0b9e
Marek Vandík <marek.vandik@gmail.com>
2026-04-23 09:18:50 +0200

    fix: goodput plotting
    Goodput is now plotted by resampling the input data into milisecond intervals and further aggregating these data - currently plotting every 20ms data point

--- graphs.py
+++ graphs.py
@@ -1,10 +1,55 @@
 #!/bin/python3
 
 import pandas as pd
+import seaborn as sns
 import matplotlib.pyplot as plt
 
-def plot_df(df):
-    pass
+def plot_cwnd(df, title, filename):
+    df['time_s'] = df['time'] / (10 ** 9)
+
+    plt.figure()
+    ax = sns.lineplot(data=df, x='time_s', y='cwnd')
+    ax.set_title(title)
+    fig = ax.get_figure()
+    fig.savefig('./results/' + filename + '-cwnd.svg')
+    plt.close(fig)
+
+
+# expects 'acked' column
+def plot_goodput(df, title, filename):
+    # resample for every ms
+    df['time'] = pd.to_datetime(df['time'], unit='ns')
+    df = df.set_index('time')
+    df = df.resample('ms').first().interpolate()
+    df['time'] = df.index.astype('int64') / (10 ** 9)
+    df = df.reset_index(drop=True)
+
+    # resolution to every 20 ms
+    df_small = df.groupby(df.index // 20).agg({
+        'time': 'first',
+        'acked': 'first'
+    }).reset_index(drop=True)
+    df = df_small
+
+    df['dt'] = df['time'].diff()
+    df['da'] = df['acked'].diff()
+
+    # correction for bpf probe - una is u32 and overflows on some occasios
+    da_wrap = df['da'] < 0
+    da_correction = df['da'].copy()
+    da_correction[da_wrap] += 2 ** 32
+
+    df['mbps'] = (da_correction / df['dt']) * 8 / (1024 * 1024)
+    mean_mbps = df['mbps'].mean()
+
+    plt.figure()
+    ax = sns.lineplot(data=df, x='time', y='mbps')
+    ax.set_title(title)
+    ax.axhline(mean_mbps, color='orange', linestyle='--', label=f'avg goodput = {mean_mbps:.2f} Mbps')
+    ax.legend()
+    fig = ax.get_figure()
+    fig.savefig('./results/' + filename + '-rate.svg')
+    plt.close(fig)
 
 def plot_quic(quic_filename):
     data = {}
@@ -26,66 +71,38 @@ def plot_quic(quic_filename):
 
                 data.setdefault(kv[0], []).append(int(kv[1]))
 
-    #print(data)
     df = pd.DataFrame.from_dict(data)
     df = df[df['sid'] == df['sid'].mode()[0]]
-    print(df)
+    df = df.dropna()
 
-    df['time'] = (df['time'] - df['time'].iloc[0]) / 1000000000
+    df['time'] = df['time'] - df['time'].iloc[0]
 
-    ax = df.plot(x='time', y='cwnd')
-    fig = ax.get_figure()
-    fig.savefig('./results/' + quic_filename + '-cwnd.svg')
-
-    df['acked_diff'] = df['acked'].diff()
-    df['time_diff'] = df['time'].diff()
-    df['mbps_rate'] = (df['acked_diff'] / df['time_diff']) * 8 / (1024 * 1024)
-    df['mbps_rate'] = df['mbps_rate'].ewm(span=10).mean()
-    #df['mbps_rate'] = df['mbps_rate'].ewm(span=30).mean()
-
-    ax = df.plot(x='time', y='mbps_rate')
-    fig = ax.get_figure()
-    fig.savefig('./results/' + quic_filename + '-rate.svg')
-
+    plot_cwnd(df, 'CWND: ' + quic_filename, quic_filename)
+    plot_goodput(df, 'Goodput: ' + quic_filename, quic_filename)
 
 
 def plot_tcp(tcp_filename):
     df = pd.read_csv('./results/' + tcp_filename, sep=',')
     df = df[df['sport'] == 80]
-    #print(df)
 
     max_port = df['dport'].mode()[0]
-    #print(max_port)
 
     df = df[df["dport"] == max_port]
-    #print(df)
 
-    df['time'] = (df['time'] - df['time'].iloc[0]) / 1000000000
+    df = df.dropna()
 
-    ax = df.plot(x='time', y='cwnd')
-    fig = ax.get_figure()
-    fig.savefig('./results/' + tcp_filename + '-cwnd.svg')
+    df['time'] = df['time'] - df['time'].iloc[0]
+    df['acked'] = df['una']
 
-    df['una_diff'] = df['una'].diff()
-    una_diff_wrap = df['una_diff'] < 0
-    una_diff_correction = df['una_diff'].copy()
-    una_diff_correction[una_diff_wrap] += 2 ** 32
-    df['time_diff'] = df['time'].diff()
-    df['mbps_rate'] = (una_diff_correction / df['time_diff']) * 8 / (1024 * 1024)
-    df['mbps_rate'] = df['mbps_rate'].ewm(span=10).mean()
-    #df['mbps_rate'] = df['mbps_rate'].ewm(span=30).mean()
-
-    ax = df.plot(x='time', y='mbps_rate')
-    fig = ax.get_figure()
-    fig.savefig('./results/' + tcp_filename + '-rate.svg')
-
+    plot_cwnd(df, 'CWND: ' + tcp_filename, tcp_filename)
+    plot_goodput(df, 'Goodput: ' + tcp_filename, tcp_filename)
 
 
 def main():
-    plot_tcp('BBR-WIFI')
-    plot_tcp('RENO-WIFI')
-    plot_tcp('CUBIC-WIFI')
-    plot_quic('QUIC-WIFI')
+    plot_tcp('BBR-wifi-topo')
+    plot_tcp('RENO-wifi-topo')
+    plot_tcp('CUBIC-wifi-topo')
+    plot_quic('QUIC-wifi-topo')
 
 if __name__ == "__main__":
     main()
